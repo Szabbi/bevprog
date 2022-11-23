ae309a901d6000570fbaff1ea3cf8cee9a7b92e3
Szabbi <nagy.szabolcsffs@gmail.com>
2022-11-23 19:42:41 +0100

    fixed hw

--- homework/sixth/main.py
+++ homework/sixth/main.py
@@ -1,31 +1,31 @@
-import os
 import string
+import sys
 if __name__ == '__main__':
-    # with open('/home/szabbi/Documents/Github/bevprog/homework/sixth/hazi.txt', 'r') as txt:
-    #    content = txt.readline()
-    # while content:
-    #    content = txt.readline()
+    with open('hazi', 'r', encoding="UTF-8") as txt:
+        content = txt.readlines()
 
     vowelsList = ['a', 'e', 'i', 'o', 'u', 'á',
                   'é', 'í', 'ú', 'ó', 'ö', 'ő', 'ü', 'ű']
-    content = "asdaqqéáeégéásdf;;..\n\na,,q"
 
-    def a(txt):
-        stripped = ""
-        for x in txt:
-            if x not in string.punctuation:
-                stripped += x.lower()
+    def b(txt):
+        stripped = []
+        # repeat for each line in input
+        for x in range(len(txt)):
+            # remove punctuation
+            for y in string.punctuation:
+                txt[x] = txt[x].replace(y, "")
+            # remove vowels upper- and lowercase
+            for z in vowelsList:
+                txt[x] = txt[x].replace(z, "")
+                txt[x] = txt[x].replace(z.upper(), "")
+            # append not empty lines to list
+            if not txt[x].isspace():
+                stripped.append(txt[x])
         return stripped
 
-    def vowels(txt):
-        noVowels = ""
-        for x in txt:
-            if x not in vowelsList:
-                noVowels += x.lower()
-        return noVowels
-
-    stripOne = a(content)
-    stripTwo = vowels(stripOne)
-
-    with open('/home/szabbi/Documents/Github/bevprog/homework/sixth/hazi.txt', 'w') as
-    print(stripTwo)
+    with open('output.txt', 'w') as out:
+        # repeat for list lenght and output if line index divisible by 3
+        for i in range(len(b(content))):
+            if i % 3 == 0:
+                out.write(b(content)[i])
+# remove mgh, empty line, punct., output every 3rd line
